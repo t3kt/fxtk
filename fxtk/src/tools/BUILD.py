b76435f77cfa436c5dc581b1369fc4072510d618
t3kt <tekt@immerse.studio>
2022-07-09 13:19:09 -0700

    palette build stuff

---
+++ fxtk/src/tools/BUILD.py
@@ -0,0 +1,18 @@
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from infraBuild import BuildTaskContext
+	from _stubs import *
+
+context = args[0]  # type: BuildTaskContext
+
+def runStage(stage: int):
+	if stage == 0:
+		context.log('Processing libraryIndex')
+		o = op('libraryIndex')
+		context.detachTox(o)
+		context.disableCloning(o)
+		context.queueCall(lambda: runStage(stage + 1))
+	elif stage == 1:
+		context.log('Processing palette')
+		context.runBuildScript(op('libraryPalette/BUILD'), thenRun=lambda: runStage(stage + 1))
